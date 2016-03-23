583a204f22796b1027ea1f57862d5a05feb58e8d
Neragoth <mobitop71@gmail.com>
2016-03-23 14:35:51 +0100

    Ajout dans un second fichier du système de fenêtre

--- Client/main - Copie.cpp
+++ Client/main - Copie.cpp
@@ -38,13 +38,13 @@ int main()
 
     cout << "Client start" << endl;
 
-    do
+    /*do
     {
         socket = new sf::TcpSocket();
 
         //Demande des infos de connexion
 
-        /*cout << "Entrez IP : ";
+        cout << "Entrez IP : ";
         getline(cin, ip);
         cout << "Entrez port : ";
         getline(cin, portString);
@@ -53,7 +53,7 @@ int main()
         cout << "Entrez port : ";
         getline(cin, portString);
         stringstream buffer(portString);
-        buffer >> port;*/
+        buffer >> port;
 
         cout << "Entrez Pseudo : ";
         getline(cin, pseudo);
@@ -82,7 +82,7 @@ int main()
             cout << "Le pseudo est deja utilise" << endl;
         }
     }
-    while(code == 1);
+    while(code == 1);*/
 
 
     if(code==101)
@@ -122,10 +122,53 @@ int main()
     //if(code == )
     // Create the main SFML window
     sf::RenderWindow window( sf::VideoMode( 800, 600 ), "SFGUI Box Example", sf::Style::Titlebar | sf::Style::Close );
+
+    // We have to do this because we don't use SFML to draw.
     window.resetGLStates();
 
+    // Create an SFGUI. This is required before doing anything with SFGUI.
+    sfg::SFGUI sfgui;
+
+    // Create our main SFGUI window
+    auto app_window = sfg::Window::Create();
+    app_window->SetTitle( "Title" );
 
 
+
+
+    sfg::Table::Ptr table = sfg::Table::Create();
+    auto button1 = sfg::Button::Create();
+    auto label1 = sfg::Label::Create();
+    auto label2 = sfg::Label::Create();
+    auto label3 = sfg::Label::Create();
+    auto entry1 = sfg::Entry::Create();
+    auto entry2 = sfg::Entry::Create();
+    auto entry3 = sfg::Entry::Create();
+
+    label1->SetText( "Adresse :" );
+    label2->SetText( "Port :" );
+    label3->SetText( "Pseudo :" );
+    entry1->SetRequisition( sf::Vector2f(120.f, 0.f ));
+    entry1->SetMaximumLength(15);
+    entry2->SetRequisition( sf::Vector2f(80.f, 0.f ));
+    entry2->SetMaximumLength(5);
+    entry3->SetRequisition( sf::Vector2f(80.f, 0.f ));
+    button1->SetLabel( "Connexion" );
+    button1->GetSignal( sfg::Widget::OnLeftClick ).Connect(std::bind(&OnButtonClick));
+
+
+    table->Attach( label1, sf::Rect<sf::Uint32>( 0, 0, 1, 1 ), sfg::Table::FILL | sfg::Table::EXPAND, sfg::Table::FILL, sf::Vector2f(0.f, 0.f));
+    table->Attach( entry1, sf::Rect<sf::Uint32>( 1, 0, 1, 1 ), sfg::Table::FILL | sfg::Table::EXPAND, sfg::Table::FILL, sf::Vector2f(0.f, 0.f));
+    table->Attach( label2, sf::Rect<sf::Uint32>( 0, 1, 1, 1 ), sfg::Table::FILL | sfg::Table::EXPAND, sfg::Table::FILL, sf::Vector2f(0.f, 0.f));
+    table->Attach( entry2, sf::Rect<sf::Uint32>( 1, 1, 1, 1 ), sfg::Table::FILL | sfg::Table::EXPAND, sfg::Table::FILL, sf::Vector2f(0.f, 0.f));
+    table->Attach( label3, sf::Rect<sf::Uint32>( 0, 2, 1, 1 ), sfg::Table::FILL | sfg::Table::EXPAND, sfg::Table::FILL, sf::Vector2f(0.f, 0.f));
+    table->Attach( entry3, sf::Rect<sf::Uint32>( 1, 2, 1, 1 ), sfg::Table::FILL | sfg::Table::EXPAND, sfg::Table::FILL, sf::Vector2f(0.f, 0.f));
+    table->Attach( button1, sf::Rect<sf::Uint32>( 0, 3, 2, 1 ), sfg::Table::FILL | sfg::Table::EXPAND, sfg::Table::FILL, sf::Vector2f(0.f, 0.f));
+
+    table->SetRowSpacings( 10.f );
+    table->SetColumnSpacings( 10.f );
+
+    app_window->Add( table );
 
     // Start the game loop
     while ( window.isOpen() )
@@ -135,6 +178,9 @@ int main()
 
         while ( window.pollEvent( event ) )
         {
+            // Handle events
+            app_window->HandleEvent( event );
+
             // Close window : exit
             if ( event.type == sf::Event::Closed )
             {
@@ -146,9 +192,13 @@ int main()
             }
         }
 
+        // Update the GUI, note that you shouldn't normally
+        // pass 0 seconds to the update method.
+        app_window->Update( 0.f );
 
         window.clear();
 
+        sfgui.Display( window );
 
         window.display();
     }
